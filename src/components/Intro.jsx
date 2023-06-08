ce651389faf95d96386aaeff5b3f8190529e9db5
Zach Gallman <zach.ecab@gmail.com>
2023-06-08 17:42:09 -0400

    Imported custom index.css styles in intro.jsx file

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Image from "next/image";
 import emoji from "../public/images/emoji-sticker.png";
+import "../styles/intro.css"
+
 
 const Intro = () => {
   return (
