07e0be754428c1b44bb14e665483c088865d3c2b
Zach Gallman <zach.ecab@gmail.com>
2023-06-10 11:57:38 -0400

    Finished converting to tailwind, custom styling next

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,61 +1,57 @@
 import React from "react";
-import "../styles/globals.css";
 
 const Contact = () => {
   return (
-    <div className="contact-section py-5">
-      <h2 className="main-text text-center" id="Contact">
+    <div className="py-20 bg-purple-700">
+      <h2 className="text-center" id="Contact">
         Contact Me
       </h2>
-      <div className="card main-text mx-auto" style={{ maxWidth: "65%" }}>
-        <div className="card-body">
-          <form
-            className="form-text"
-            action="https://formsubmit.co/zach.ecab@gmail.com"
-            method="POST"
-          >
-            <div className="mb-3">
-              <label htmlFor="email" className="form-label">
-                Email address
-              </label>
-              <input
-                type="email"
-                className="form-control"
-                id="email"
-                aria-describedby="email"
-                required
-                placeholder="Email"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="name" className="form-label">
-                Name
-              </label>
-              <input
-                type="text"
-                className="form-control"
-                id="name"
-                required
-                placeholder="Name"
-              />
-            </div>
-            <div className="mb-3">
-              <label htmlFor="message" className="form-label">
-                Message
-              </label>
-              <textarea
-                className="form-control"
-                id="message"
-                name="message"
-                placeholder="Message"
-                style={{ minHeight: "10rem" }}
-              ></textarea>
-            </div>
-            <button type="submit" className="btn button-gradient submit-button">
-              Submit
-            </button>
-          </form>
-        </div>
+      <div className="mx-auto w-full lg:w-1/2 p-6 bg-white rounded-lg shadow-lg">
+        <form
+          action="https://formsubmit.co/zach.ecab@gmail.com"
+          method="POST"
+        >
+          <div className="mb-4">
+            <label htmlFor="email" className="block mb-2">
+              Email address
+            </label>
+            <input
+              type="email"
+              className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              id="email"
+              aria-describedby="email"
+              required
+              placeholder="Email"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="name" className="block mb-2">
+              Name
+            </label>
+            <input
+              type="text"
+              className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              id="name"
+              required
+              placeholder="Name"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="message" className="block mb-2">
+              Message
+            </label>
+            <textarea
+              className="w-full px-3 py-2 placeholder-gray-300 border border-gray-300 rounded-md focus:outline-none focus:border-indigo-500"
+              id="message"
+              name="message"
+              placeholder="Message"
+              style={{ minHeight: "10rem" }}
+            ></textarea>
+          </div>
+          <button type="submit" className="w-full px-3 py-2 text-white bg-indigo-500 rounded-md hover:bg-indigo-600 focus:outline-none">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,52 +1,37 @@
 import React from "react";
 import "@fortawesome/fontawesome-free/css/all.css";
-import "../styles/globals.css";
-import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer className="footer-section text-center">
-      <div className="container">
-        <div className="row">
-          <div className="col-6 text-center">
-            <h5 className="main-text">Socials</h5>
-            <div className="social-icons">
-              <Link
-                className="social-icon"
-                href="https://www.instagram.com/zach.g33/"
-              >
-                <i className="fab fa-instagram main-text"></i>
-              </Link>
-              <Link className="social-icon" href="https://github.com/zdotg">
-                <i className="fab fa-github main-text"></i>
-              </Link>
-              <Link
-                className="social-icon"
-                href="https://www.linkedin.com/in/zach-gallman/"
-              >
-                <i className="fab fa-linkedin main-text"></i>
-              </Link>
+    <footer className="text-center bg-purple-700 py-6">
+      <div className="container mx-auto px-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          <div className="text-center">
+            <h5 className="text-xl font-semibold mb-4">Socials</h5>
+            <div className="flex justify-center space-x-4">
+              <div href="https://www.instagram.com/zach.g33/">
+                <a className="text-2xl"><i className="fab fa-instagram"></i></a>
+              </div>
+              <div href="https://github.com/zdotg">
+                <a className="text-2xl"><i className="fab fa-github"></i></a>
+              </div>
+              <div href="https://www.linkedin.com/in/zach-gallman/">
+                <a className="text-2xl"><i className="fab fa-linkedin"></i></a>
+              </div>
             </div>
           </div>
-          <div className="col-6 text-center">
-            <div className="contact-info">
-              <Link
-                role="button"
-                className="contact-link main-text"
-                href="tel:+13347181224"
-              >
-                <i className="fas fa-phone"></i> 1-334-718-1224
-              </Link>
-              <br />
-              <Link
-                role="button"
-                className="contact-link main-text"
-                href="mailto:zach.ecab@gmail.com"
-              >
-                <i className="fas fa-envelope main-text"></i>{" "}
-                zach.ecab@gmail.com
-              </Link>
-              <br />
+          <div className="text-center">
+            <div className="contact-info mt-4 md:mt-0">
+              <div href="tel:+13347181224">
+                <a role="button" className="contact-link block text-lg my-2">
+                  <i className="fas fa-phone"></i> 1-334-718-1224
+                </a>
+              </div>
+              <div href="mailto:zach.ecab@gmail.com">
+                <a role="button" className="contact-link block text-lg my-2">
+                  <i className="fas fa-envelope"></i> zach.ecab@gmail.com
+                </a>
+              </div>
             </div>
           </div>
         </div>
