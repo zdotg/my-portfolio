49aad38c154ceee42db28c1e157414f1a7959e83
Zach Gallman <zach.ecab@gmail.com>
2023-06-15 14:11:05 -0400

    Inverted nav text color and changed background to white

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,10 +11,10 @@ const NavBar = () => {
   };
 
   return (
-    <nav className="bg-purple-700 p-4">
+    <nav className="bg-white p-4">
       <div className="container mx-auto px-4 flex justify-between items-center">
         <Link href="/">
-          <div className="text-3xl font-bold text-white cursor-pointer">
+          <div className="text-3xl font-bold text-black cursor-pointer">
             Zach <span className="text-gradient">Gallman</span>;
           </div>
         </Link>
@@ -22,28 +22,28 @@ const NavBar = () => {
           className="md:hidden"
           onClick={toggleMenu}
         >
-          <span className="fas fa-bars text-white"></span>
+          <span className="fas fa-bars text-black"></span>
         </button>
         <div className={`md:flex items-center ${isOpen ? '' : 'hidden'}`}>
           <ul className="md:flex space-x-4">
             <li className="nav-item">
               <Link href="#About">
-                <div className="text-white cursor-pointer">About Me</div>
+                <div className="text-black cursor-pointer">About Me</div>
               </Link>
             </li>
             <li className="nav-item">
               <Link href="#Skills">
-                <div className="text-white cursor-pointer">Skills & Certifications</div>
+                <div className="text-black cursor-pointer">Skills & Certifications</div>
               </Link>
             </li>
             <li className="nav-item">
               <Link href="#Portfolio">
-                <div className="text-white cursor-pointer">Portfolio</div>
+                <div className="text-black cursor-pointer">Portfolio</div>
               </Link>
             </li>
             <li className="nav-item">
               <Link href="#Contact">
-                <div className="text-white cursor-pointer">Contact Me</div>
+                <div className="text-black cursor-pointer">Contact Me</div>
               </Link>
             </li>
           </ul>
