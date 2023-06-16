92ad5f32432e95a2a8fa8fa59f2186dc8c705de0
Zach Gallman <zach.ecab@gmail.com>
2023-06-16 12:20:27 -0400

    Changed the div elements for contact and social buttons to anchor elements

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,30 +8,25 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="text-center">
             <h5 className="text-xl font-semibold mb-4">Socials</h5>
+            {/* Socials */}
             <div className="flex justify-center space-x-4">
-              <div href="https://www.instagram.com/zach.g33/">
-                <a className="text-2xl"><i className="fab fa-instagram"></i></a>
-              </div>
-              <div href="https://github.com/zdotg">
-                <a className="text-2xl"><i className="fab fa-github"></i></a>
-              </div>
-              <div href="https://www.linkedin.com/in/zach-gallman/">
-                <a className="text-2xl"><i className="fab fa-linkedin"></i></a>
-              </div>
+              <a href="https://www.instagram.com/zach.g33/" role="button" className="text-2xl"><i className="fab fa-instagram"></i></a>
+
+              <a href="https://github.com/zdotg" role="button" className="text-2xl"><i className="fab fa-github"></i></a>
+
+              <a href="https://www.linkedin.com/in/zach-gallman/" role="button" className="text-2xl"><i className="fab fa-linkedin"></i></a>
             </div>
           </div>
+          {/* Email and Phone Section */}
           <div className="text-center">
             <div className="contact-info mt-4 md:mt-0">
-              <div href="tel:+13347181224">
-                <a role="button" className="contact-link block text-lg my-2">
-                  <i className="fas fa-phone"></i> 1-334-718-1224
-                </a>
-              </div>
-              <div href="mailto:zach.ecab@gmail.com">
-                <a role="button" className="contact-link block text-lg my-2">
-                  <i className="fas fa-envelope"></i> zach.ecab@gmail.com
-                </a>
-              </div>
+              <a href="tel:+13347181224" role="button" className="contact-link block text-lg my-2">
+                <i className="fas fa-phone"></i> 1-334-718-1224
+              </a>
+
+              <a href="mailto:zach.ecab@gmail.com" role="button" className="contact-link block text-lg my-2">
+                <i className="fas fa-envelope"></i> zach.ecab@gmail.com
+              </a>
             </div>
           </div>
         </div>
