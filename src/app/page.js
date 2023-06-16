66da8c80428bc7aba3b204f263a7b126a8e28b7d
Zach Gallman <zach.ecab@gmail.com>
2023-06-16 16:31:42 -0400

    Revert

--- src/app/page.js
+++ src/app/page.js
@@ -1,39 +1,39 @@
-"use client";
+// "use client";
 
-import "../styles/globals.css";
-import React from "react";
-import Head from "next/head";
-import NavBar from "@/components/NavBar";
-import Intro from "@/components/Intro";
-import About from "@/components/About";
-import Experience from "@/components/Experience";
-import Skills from "@/components/Skills";
-import Certs from "@/components/Certs";
-import Projects from "@/components/Projects";
-import Contact from "../components/Contact";
-import Footer from "@/components/Footer";
-// Import other components as needed
+// import "../styles/globals.css";
+// import React from "react";
+// import Head from "next/head";
+// import NavBar from "@/components/NavBar";
+// import Intro from "@/components/Intro";
+// import About from "@/components/About";
+// import Experience from "@/components/Experience";
+// import Skills from "@/components/Skills";
+// import Certs from "@/components/Certs";
+// import Projects from "@/components/Projects";
+// import Contact from "../components/Contact";
+// import Footer from "@/components/Footer";
+// // Import other components as needed
 
-export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>Zach Gallman Portfolio</title>
-        {/* Include other meta tags, stylesheets, or scripts */}
-      </Head>
-      <NavBar />
-      <main>
-        <Intro />
-        <Experience />
-        <About />
-        <Skills />
-        <Certs />
-        <Projects />
-        <Contact />
-        {/* Render other components */}
-      </main>
-      <Footer />
-      {/* Include a footer component if needed */}
-    </div>
-  );
-}
+// export default function Home() {
+//   return (
+//     <div>
+//       <Head>
+//         <title>Zach Gallman Portfolio</title>
+//         {/* Include other meta tags, stylesheets, or scripts */}
+//       </Head>
+//       <NavBar />
+//       <main>
+//         <Intro />
+//         <Experience />
+//         <About />
+//         <Skills />
+//         <Certs />
+//         <Projects />
+//         <Contact />
+//         {/* Render other components */}
+//       </main>
+//       <Footer />
+//       {/* Include a footer component if needed */}
+//     </div>
+//   );
+// }
